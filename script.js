5599bebdb39207d0457bef8d12591f1b39140c85
sterific <75123700+sterific@users.noreply.github.com>
2021-02-28 20:06:44 +0530

    Guessing Game
    This is number guessing game where the player have to choose no. between 1 and 20. If the guessed no. matches the actual no. then the player wins.
    Max. try limit is 20.

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+let number=Math.trunc(Math.random()*20)+1;
+let score =20;
+let highscore=0;
+
+const displayMessage = function (message) 
+{
+    document.querySelector('.message').textContent = message;
+};
+
+
+document.querySelector(".check").addEventListener("click",function(){
+    const guess=Number(document.querySelector(".guess").value);
+    if(guess===number)
+    {
+        displayMessage('🎉 Correct Number!');
+        document.querySelector('.number').textContent = number;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+          }
+    }
+    else if (!guess) 
+    {
+        displayMessage('⛔️ No number!');
+    }
+    else if(guess !== number)
+    {
+        if (score > 1) 
+        {
+            displayMessage(guess > number ? '📈 Too high!' : '📉 Too low!');
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else 
+        {
+            displayMessage('💥 You lost the game!');
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    number = Math.trunc(Math.random() * 20) + 1;
+
+    displayMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+  
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+  });
